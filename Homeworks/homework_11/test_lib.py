d8f659d354a8c72e0901a0a5c094843fb16abfd5
viktor_ozerchuk <ozerchuk22@ukr.net>
2023-03-14 15:29:06 +0200

    renew homework_11

--- Homeworks/homework_11/test_lib.py
+++ Homeworks/homework_11/test_lib.py
@@ -2,48 +2,28 @@ import pytest
 import lib
 
 
-def test1() -> None:
-    """
-    Checking whether a function returns a string
-    """
-    assert isinstance(lib.generate_random_string(), str)
+def test_generate_random_number_in_range() -> None:
+    num = lib.generate_random_number()
+    assert isinstance(num, int)
+    assert -10 ** 10 <= num <= 10 ** 10
 
 
-def test2() -> None:
-    """
-    Check that the generate_random_string function works with parameter 20
-    """
+def test_generate_random_number_out_range() -> None:
+    assert lib.generate_random_number() < -10 ** 10
+
+
+def test_get_float_num() -> None:
+    with pytest.raises(TypeError):
+        assert isinstance(lib.generate_random_string(2.2), str)
+
+
+def test_get_int_num() -> None:
     assert isinstance(lib.generate_random_string(20), str)
 
 
-def test3() -> None:
-    """
-    Check that the generate_random_string function works with parameter -1
-    """
-    assert isinstance(lib.generate_random_string(-1), str)
+def test_get_negative_length() -> None:
+    assert lib.generate_random_string(-1) == ''
 
 
-def test4() -> None:
-    """
-    Check that the generate_random_string function works with parameter 5.5
-    """
-    assert isinstance(lib.generate_random_string(5.5), str)
-
-
-def test5() -> None:
-    """
-    Check that the generate_random_string function works with parameter 5.5
-    """
+def test__zero_length() -> None:
     assert len(lib.generate_random_string(0)) == 0
-
-
-def test6() -> None:
-    """
-    Check that the generate_random_string function works with parameter 5.5
-    """
-    assert len(lib.generate_random_string(1)) == 1
-
-
-
-
-
