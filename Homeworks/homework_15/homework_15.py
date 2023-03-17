bd2e878a8168506345a560662f88f22850de4fbf
viktor_ozerchuk <ozerchuk22@ukr.net>
2023-03-17 19:47:06 +0200

    fix homework 15

--- Homeworks/homework_15/homework_15.py
+++ Homeworks/homework_15/homework_15.py
@@ -1,22 +1,23 @@
 import requests
 import datetime
+from json.decoder import JSONDecodeError
 
 
 def get_exchange_rates():
     url = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json1212'
     response = requests.get(url)
-    data = response.json()  # unpack JSON
-
-    if response.status_code != 200:
-        raise ValueError(f'Problem to get the exchange rate from NBU. Status code: {response.status_code}')
+    try:
+        data = response.json()
+    except JSONDecodeError:  # in this case we can use JSONDecodeError if the response is not a valid JSON
+        raise ValueError('Invalid JSON response')
 
     with open('exchange_rates.txt', 'w') as file:  # open the file for write
         date_creation = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # write date of creation of the request
         file.write(f'[{date_creation}]\n')
 
         for currency in data:  # write the exchange rate of each currency
-            name = currency['cc']
-            rate = currency['rate']
+            name = currency.get('cc')
+            rate = currency.get('rate')
             file.write(f'{name} to UAH: {rate}\n')
 
 
