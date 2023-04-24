332dd935ad5cbd17024fceca37fe03b96d6f075c
viktor_ozerchuk <ozerchuk22@ukr.net>
2023-04-24 17:52:35 +0300

    rename

--- Homeworks/homework_13/pack/test_laptop_screen_refresh_rate.py
+++
@@ -1,7 +0,0 @@
-def test_laptop_screen_refresh_rate(laptop):
-    assert laptop.screen_refresh_rate == 120
-
-
-def test_laptop_screen_refresh_rate_(laptop):
-    laptop.screen_refresh_rate += 100
-    assert laptop.screen_refresh_rate == 60
