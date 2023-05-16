5d2708be78ef6d280d3b56561ae2870981dbde1a
viktor_ozerchuk <ozerchuk22@ukr.net>
2023-05-16 13:43:44 +0300

    new fixes

--- Homeworks/homework_19/tests/test_radiobutton_page.py
+++ Homeworks/homework_19/tests/test_radiobutton_page.py
@@ -5,8 +5,14 @@ from Homeworks.homework_19.pages.RadiobuttonPage import RadioButtonPage
 
 @pytest.mark.usefixtures('chrome')
 class TestRadiobuttonPage:
-    def test_radiobutton(self):
+    def test_activate_yes_button(self):
         page = RadioButtonPage(self.driver)
         page.open()
-        page.radio_button_is_selected('yes')
         page.activate_yes_button('yes')
+
+    def test_radio_button_is_selected(self):
+        page = RadioButtonPage(self.driver)
+        page.radio_button_is_selected('yes')
+
+
+
